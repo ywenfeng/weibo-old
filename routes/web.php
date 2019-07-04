232c9e329ce8bf28af21cc530a8b8c089f713562
yangwenfeng <913013106@qq.com>
2019-07-04 17:45:23 +0000

    创建会话

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,8 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('/signup', 'UsersController@create')->name('signup');
 
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
+
 Route::resource('users','UsersController');
